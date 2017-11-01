863d100d7d021299f9fa3c95e385c351ba0abea9
Daniel Torok <sam@rarity.hu>
2017-11-01 21:52:09 +0100

    paddle, ball and colliders

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    private bool started = false;
+    private Ball ball;
+    
+    private float xmin;
+    private float xmax;
+
+	// Use this for initialization
+	void Start () {
+        ball = FindObjectOfType<Ball>() as Ball;
+
+        // float cameraDistance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
+
+        float size = GetComponent<Collider2D>().bounds.size.x;
+        xmin = leftmost.x + size / 2;
+        xmax = rightmost.x - size / 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float x = Mathf.Clamp(cursorPos, xmin, xmax);
+        transform.position = new Vector3(x, transform.position.y);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!started) {
+            started = true;
+            ball.GetComponent<Rigidbody2D>().velocity = new Vector3(5, 5, 0);
+        }
+    }
+}
